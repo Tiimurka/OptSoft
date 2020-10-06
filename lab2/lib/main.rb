1000d750071db889c01ef6c5b2f401273527a478
Tiimurka <timur22211@gmail.com>
2020-10-06 18:22:05 +0700

    Added error handler for filename in lab2

--- lab2/lib/main.rb
+++ lab2/lib/main.rb
@@ -3,9 +3,20 @@
 require_relative 'parse'
 
 class Main
+  def self.file_name_handler(str)
+    # puts str
+    puts str.scan(/\..../)
+    return 0 if str.end_with?('.csv')
+
+    puts 'Не тот формат файла, должен быть .csv'
+    -1
+  end
+
   def self.init_file
     puts 'Введите название файла (он должен быть в формате .csv!):'
     filepath = gets.chomp
+    return -1 if file_name_handler(filepath) == -1
+
     CSVcalc.new(filepath)
   end
 
@@ -20,6 +31,8 @@ class Main
 
   def self.begin
     csv_data = init_file
+    return if csv_data == -1
+
     puts 'Введите номер столбца для осуществления вычислений'
     col = ch_cols(csv_data.data, csv_data.cols)
     puts 'Минимальное значение = '"#{csv_data.csv_min(col)}"
